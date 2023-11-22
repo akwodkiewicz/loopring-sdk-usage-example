06fd0eb1b1e8b3fea5633890597287c19cf2c524
Andrzej Wódkiewicz <akwodkiewicz@gmail.com>
2023-11-22 17:14:37 +0100

    add other dynamic import that's working

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
+// just an example import that is eventually transpiled into a `require` if the project is CJS
 import { info } from "node:console";
 
 async function main() {
   info("Hello, World!");
-  /**
-   * This line causes a runtime error:
-   *    ReferenceError: module is not defined in ES module scope
-   */
-  const library = await import("@loopring-web/loopring-sdk");
+
+  // Dynamic import of an ESM library (it's impossible to import it statically)
+  const nodeFetch = await import("node-fetch");
+  info("imported default:", nodeFetch);
+
+  // Dynamic import of @loopring-web/loopring-sdk that claims to be ESM
+  const library = await import("@loopring-web/loopring-sdk"); // runtime error
+  info("imported default:", library);
 }
 
 main().then(() => {
