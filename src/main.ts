0537e940276c82a266bab33ddb9986010bf974f1
Andrzej Wódkiewicz <akwodkiewicz@gmail.com>
2023-11-22 17:18:33 +0100

    convert the app to ESM

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 // just an example import that is eventually transpiled into a `require` if the project is CJS
 import { info } from "node:console";
-
+import * as nodeFetch from "node-fetch";
+import * as sdk from "@loopring-web/loopring-sdk";
 async function main() {
   info("Hello, World!");
 
-  // Dynamic import of an ESM library (it's impossible to import it statically)
-  const nodeFetch = await import("node-fetch");
-  info("imported default:", nodeFetch);
-
-  // Dynamic import of @loopring-web/loopring-sdk that claims to be ESM
-  const library = await import("@loopring-web/loopring-sdk"); // runtime error
-  info("imported default:", library);
+  info("imported nodeFetch:", nodeFetch);
+  info("imported loopringSdk:", sdk);
 }
 
 main().then(() => {
